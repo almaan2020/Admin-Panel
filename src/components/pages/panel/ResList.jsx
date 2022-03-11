c7f6aeed78127368a319d85686e1b7b51cb16507
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-03-11 13:04:45 +0330

    Update ResList.jsx

--- src/components/pages/panel/ResList.jsx
+++ src/components/pages/panel/ResList.jsx
@@ -1,39 +1,87 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getlist } from "../../../store/slices/resourceSlice";
+import {
+  Link,
+  useNavigate,
+  createSearchParams,
+  useSearchParams,
+} from "react-router-dom";
+import { Image } from "react-bootstrap";
+import { getlist, deleteUser } from "../../../store/slices/userSlice";
 import List from "../../common/List";
 
-const ResList = () => {
+const UserList = () => {
+  const { userList, status, total } = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const { resourceList, status } = useSelector((state) => state.resource);
+  const navigate = useNavigate();
 
+  const [searchParams] = useSearchParams();
   useEffect(() => {
-    if (status === "idle") {
-      dispatch(getlist());
-    }
-  }, [dispatch, status]);
+    const { page } = Object.fromEntries([...searchParams]);
+    dispatch(getlist(page));
+  }, [dispatch, searchParams]);
+
+  const rowLink = (field) => (user) => {
+    const params = { id: user.id };
+    return (
+      <Link
+        to={`/panel/user-detail?${createSearchParams(params)}`}
+        className="no-underline"
+      >
+        {user[field]}
+      </Link>
+    );
+  };
 
   const columns = [
-    { path: "id", label: "ID" },
-    { path: "name", label: "Name" },
-    { path: "year", label: "Year" },
-    { path: "color", label: "Color" },
-    { path: "pantone_value", label: "Pantone" },
     {
-      key: "colorDiv",
-      content: (resource) => (
-        <div
-          style={{
-            width: "10px",
-            height: "10px",
-            backgroundColor: `${resource.color}`,
-          }}
-        />
+      path: "id",
+      label: "ID",
+      content: rowLink("id"),
+    },
+    { path: "email", label: "Email", content: rowLink("email") },
+    { path: "first_name", label: "FirstName", content: rowLink("first_name") },
+    { path: "last_name", label: "LastName", content: rowLink("last_name") },
+    {
+      key: "avatar",
+      content: (user) => <Image src={user.avatar} thumbnail />,
+    },
+    {
+      key: "edit",
+      content: (user) => {
+        const params = { id: user.id };
+        return (
+          <button
+            onClick={() =>
+              navigate(`/panel/user-edit?${createSearchParams(params)}`)
+            }
+            className="btn btn-outline-dark btn-sm"
+          >
+            <span className="fa fa-pencil"></span>
+          </button>
+        );
+      },
+    },
+    {
+      key: "delete",
+      content: (user) => (
+        <button
+          onClick={() => dispatch(deleteUser(user.id))}
+          className="btn btn-outline-dark btn-sm"
+        >
+          <span className="fa fa-trash"></span>
+        </button>
       ),
     },
   ];
-
-  return <List status={status} columns={columns} list={resourceList} />;
+  return (
+    <List
+      status={status}
+      columns={columns}
+      list={userList}
+      itemCounts={total}
+    />
+  );
 };
 
-export default ResList;
+export default UserList;
