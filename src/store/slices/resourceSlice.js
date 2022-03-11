d4c097a3c0bd5fb2b43f4cec1c64742f973379dd
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-03-11 13:03:07 +0330

    Update resourceSlice.js

--- src/store/slices/resourceSlice.js
+++ src/store/slices/resourceSlice.js
@@ -1,14 +1,25 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import resourceService from "../../services/resourceService";
 
-const initialState = { resourceList: [], status: "idle", total: 0 };
+const initialState = { resourceList: [], resourceInfo: {}, status: "idle", total: 0 };
 
 export const getlist = createAsyncThunk(
     "resource/getlist",
-    async (thunkAPI) => {
+    async (page, thunkAPI) => {
         try {
-            const response = await resourceService.getResourceList();
+            const response = await resourceService.getResourceList(page);
             return response.data.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue();
+        }
+    });
+
+export const getResource = createAsyncThunk(
+    "user/getResource",
+    async (id, thunkAPI) => {
+        try {
+            const response = await resourceService.getResource(id);
+            return response.data;
         } catch (error) {
             return thunkAPI.rejectWithValue();
         }
@@ -38,6 +49,9 @@ const resourceSlice = createSlice({
         },
         [getTotalResource.fulfilled]: (state, action) => {
             state.total = action.payload;
+        },
+        [getResource.fulfilled]: (state, action) => {
+            state.resourceInfo = { ...action.payload };
         },
     },
 });
