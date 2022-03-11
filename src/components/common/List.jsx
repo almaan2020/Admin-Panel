01fc70272d007ef23660e75c7e638d3ac38fdc91
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-03-11 13:07:21 +0330

    Update List.jsx

--- src/components/common/List.jsx
+++ src/components/common/List.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Table from "./Table";
+import Pagination from "./Pagination";
 
 const List = (props) => {
-  const { status, columns, list } = props;
+  const { status, columns, list, itemCounts, trLink } = props;
 
   let content;
   if (status === "loading")
     content = <span className="spinner-border spinner-border-sm"></span>;
   else if (status === "succeeded" && list.length !== 0)
-    content = <Table columns={columns} data={list} />;
-  else content = <span>There is no data!</span>;
+    content = (
+      <>
+        <Table columns={columns} data={list} trLink={trLink} />
+        <Pagination itemCounts={itemCounts} />
+      </>
+    );
+  else content = <span className="text-center">There is no data!</span>;
 
   return (
     <div className="table-responsive shadow-sm card border-light col-sm-8">
