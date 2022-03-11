dfb6042922970e51207ba252ced4e565aa45d921
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-03-11 13:02:34 +0330

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,13 +3,21 @@ import apiConfig from "../config/apiConfig";
 
 const userUrl = apiConfig.apiUrl + "/users";
 
-const getUserList = async () => {
-    return await http.get(userUrl);
+const getUserList = async (page) => {
+    return await http.get(`${userUrl}?page=${page}`);
+}
+
+const getUser = async (id) => {
+    return await http.get(`${userUrl}/${id}`);
 }
 
 const createUser = async (first_name, last_name, email) => {
     return await http.post(userUrl, { first_name, last_name, email });
 };
+
+const editUser = async (id, first_name, last_name, email) => {
+    return await http.put(`${userUrl}/${id}`, { first_name, last_name, email });
+}
 
 const deleteUser = async (id) => {
     await http.delete(`${userUrl}/${id}`);
@@ -17,7 +25,9 @@ const deleteUser = async (id) => {
 
 const userService = {
     getUserList,
+    getUser,
     createUser,
+    editUser,
     deleteUser,
 };
 export default userService;
