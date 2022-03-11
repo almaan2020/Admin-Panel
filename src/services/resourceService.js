9fd5553ffdd9d3ab9468db695229c914f40eecdf
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-03-11 13:02:13 +0330

    Update resourceService.js

--- src/services/resourceService.js
+++ src/services/resourceService.js
@@ -3,11 +3,16 @@ import apiConfig from "../config/apiConfig";
 
 const resUrl = apiConfig.apiUrl + "/unknown";
 
-const getResourceList = async () => {
-    return await http.get(resUrl);
+const getResourceList = async (page) => {
+    return await http.get(`${resUrl}?page=${page}`);
+}
+
+const getResource = async (id) => {
+    return await http.get(`${resUrl}/${id}`);
 }
 
 const resourceService = {
     getResourceList,
+    getResource
 };
 export default resourceService;
