8629838ef61793e85ef4d608008ea26d60fc40fd
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-03-11 13:01:30 +0330

    Update PanelRoutes.jsx

--- src/routes/PanelRoutes.jsx
+++ src/routes/PanelRoutes.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../components/pages/panel/Home";
 import NewUser from "../components/pages/panel/NewUser";
+import EditUser from "../components/pages/panel/EditUser";
 import UserList from "../components/pages/panel/UserList";
 import ResList from "../components/pages/panel/ResList";
+import UserDetail from "../components/pages/panel/UserDetail";
+import ResDetail from "../components/pages/panel/ResDetail";
 
 const PanelRoutes = () => {
   return (
@@ -13,8 +16,11 @@ const PanelRoutes = () => {
         element={<Home text="You are" textMuted=" Logged in." />}
       />
       <Route path="/user-new" element={<NewUser />} />
+      <Route path="/user-edit" element={<EditUser />} />
       <Route path="/user-list" element={<UserList />} />
       <Route path="/resource-list" element={<ResList />} />
+      <Route path="/user-detail" element={<UserDetail />} />
+      <Route path="/resource-detail" element={<ResDetail />} />
     </Routes>
   );
 };
