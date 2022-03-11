3e43ef9d17158cf0e2987eff7de34f30dab9155c
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-03-11 13:05:57 +0330

    Update Home.jsx

--- src/components/pages/panel/Home.jsx
+++ src/components/pages/panel/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 const Home = (props) => {
   const { text, textMuted } = props;
   return (
-    <h3>
-      {text}
-      <small className="text-muted">{textMuted}</small>
-    </h3>
+    <div className="text-center p-5">
+      <h3>
+        {text}
+        <small className="text-muted">{textMuted}</small>
+      </h3>
+    </div>
   );
 };
 
