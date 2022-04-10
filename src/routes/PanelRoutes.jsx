b55b0037d6294abe4fa90452f1873e02df6ae5d3
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-04-10 23:53:58 +0430

    Update PanelRoutes.jsx

--- src/routes/PanelRoutes.jsx
+++ src/routes/PanelRoutes.jsx
@@ -1,26 +1,46 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Home from "../components/pages/panel/Home";
 import NewUser from "../components/pages/panel/NewUser";
 import EditUser from "../components/pages/panel/EditUser";
 import UserList from "../components/pages/panel/UserList";
-import ResList from "../components/pages/panel/ResList";
+import ResourceList from "../components/pages/panel/ResourceList";
 import UserDetail from "../components/pages/panel/UserDetail";
-import ResDetail from "../components/pages/panel/ResDetail";
+import ResourceDetail from "../components/pages/panel/ResourceDetail";
+import routes from "../config/routes";
 
 const PanelRoutes = () => {
+  const { user: currentUser } = useSelector((state) => state.auth);
+  const {
+    panel,
+    login,
+    home,
+    userNew,
+    userEdit,
+    userList,
+    resourceList,
+    userDetail,
+    resourceDetail,
+  } = routes;
+
+  if (!currentUser) {
+    return <Navigate replace to={login} />;
+  }
   return (
     <Routes>
       <Route
-        path="/home"
+        path={home}
         element={<Home text="You are" textMuted=" Logged in." />}
       />
-      <Route path="/user-new" element={<NewUser />} />
-      <Route path="/user-edit" element={<EditUser />} />
-      <Route path="/user-list" element={<UserList />} />
-      <Route path="/resource-list" element={<ResList />} />
-      <Route path="/user-detail" element={<UserDetail />} />
-      <Route path="/resource-detail" element={<ResDetail />} />
+      <Route path={userNew} element={<NewUser />} />
+      <Route path={userEdit} element={<EditUser />} />
+      <Route path={userList} element={<UserList />} />
+      <Route path={resourceList} element={<ResourceList />} />
+      <Route path={userDetail} element={<UserDetail />} />
+      <Route path={resourceDetail} element={<ResourceDetail />} />
+      <Route path="/" element={<Navigate replace to={`${panel}${home}`} />} />
+      <Route path="*" element={<Navigate replace to="/404" />} />
     </Routes>
   );
 };
