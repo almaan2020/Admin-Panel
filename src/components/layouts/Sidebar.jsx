6625fbaac5e4e69a0a6ec274b268d2de1c3b54d5
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-04-10 23:47:38 +0430

    Update Sidebar.jsx

--- src/components/layouts/Sidebar.jsx
+++ src/components/layouts/Sidebar.jsx
@@ -5,14 +5,17 @@ import AccordionNavItem from "./AccordionNavItem";
 import NavItem from "./NavItem";
 import sidebarConfig from "../../config/sidebarConfig";
 import logo from "../../assets/panel-logo.png";
+import routes from "../../config/routes";
 
 const Sidebar = () => {
+  const { panel, home, userList, userNew, resourceList, user, resource } =
+    routes;
   return (
     <div className="px-4 pt-3">
       <div className="py-2">
         <Image src={logo} thumbnail />
         &nbsp;&nbsp;
-        <Link className="sidebar-text sidebar-title" to="/panel/home">
+        <Link className="sidebar-text sidebar-title" to={`${panel}${home}`}>
           {sidebarConfig.sidebarTitle}
         </Link>
       </div>
@@ -20,15 +23,15 @@ const Sidebar = () => {
       <Nav className="flex-column pt-3 pt-md-0">
         <Dropdown.Divider className="my-3" />
 
-        <AccordionNavItem eventKey="/user" title="User" icon="user">
-          <NavItem title="List" link="/panel/user-list" />
-          <NavItem title="Create New User" link="/panel/user-new" />
+        <AccordionNavItem eventKey={user} title="User" icon="user">
+          <NavItem title="List" link={`${panel}${userList}`} />
+          <NavItem title="Create New User" link={`${panel}${userNew}`} />
         </AccordionNavItem>
 
         <Dropdown.Divider className="my-3" />
 
-        <AccordionNavItem eventKey="/resource" title="Resource" icon="cog">
-          <NavItem title="List" link="/panel/resource-list" />
+        <AccordionNavItem eventKey={resource} title="Resource" icon="cog">
+          <NavItem title="List" link={`${panel}${resourceList}`} />
         </AccordionNavItem>
       </Nav>
     </div>
