8b7d26fa7fb8894d1e53a121934434eeae79ca2d
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-04-10 23:53:26 +0430

    Update MainRoutes.jsx

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
-import Panel from "../components/pages/Panel";
 import Register from "../components/pages/Register";
 import Login from "../components/pages/Login";
 import NotFound from "../components/pages/NotFound";
+import PanelDashboard from "../components/pages/PanelDashboard";
+import routes from "../config/routes";
 
 const MainRoutes = () => {
+  const { panel, login, register } = routes;
   return (
     <Routes>
-      <Route path="/panel/*" element={<Panel />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/" element={<Navigate replace to="/login" />} />
+      <Route path={`${panel}/*`} element={<PanelDashboard />} />
+      <Route path={login} element={<Login />} />
+      <Route path={register} element={<Register />} />
+      <Route path="/" element={<Navigate replace to={login} />} />
       <Route path="*" element={<NotFound message="Page Not Found!" />} />
     </Routes>
   );
