8776e6bb80fc53f95006e97bab09756e2acd6611
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-04-10 23:46:22 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { store } from './store/store';
+import store from './store/store';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
