6d9e76466b9aaf0ee2536b8136bd3542d13d52e2
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-04-10 23:52:54 +0430

    Create routes.js

---
+++ src/config/routes.js
@@ -0,0 +1,14 @@
+export default Object.freeze({
+    login: "/login",
+    register: "/register",
+    panel: "/panel",
+    home: "/home",
+    userNew: "/user-new",
+    userEdit: "/user-edit",
+    userList: "/user-list",
+    resourceList: "/resource-list",
+    userDetail: "/user-detail",
+    resourceDetail: "/resource-detail",
+    user: "/user",
+    resource: "/resource"
+});
